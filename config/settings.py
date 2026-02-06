96102ced861b77c38156ddfd89e0d971721692e3
Anuar U <rest.vip7@gmail.com>
2026-02-06 15:36:55 +0300

    Fix: Add Vercel domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2^n+ty!5opiul)2+3qf7x4nfr_)ckfci43%z1xyd#94lubna^r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vibe-estate.vercel.app', 'localhost', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
